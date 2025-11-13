ac3c7337cb4c23b67792e0c4a99a57f939a4bfbd
Resource Factors <it@resourcefactors.com>
2025-11-13 18:24:38 +0400

    feat: Initialize App

--- rf/hooks.py
+++ rf/hooks.py
@@ -1,15 +1,25 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from . import __version__ as app_version
-
 app_name = "rf"
 app_title = "Rf"
-app_publisher = "Resource Factors DMCC"
-app_description = "Customizations for RF Project Management on ERPNext"
-app_icon = "octicon octicon-file-directory"
-app_color = "grey"
-app_email = "developer@resourcefactors.com"
-app_license = "MIT"
+app_publisher = "Resource Factors"
+app_description = "RF"
+app_email = "it@resourcefactors.com"
+app_license = "mit"
+
+# Apps
+# ------------------
+
+# required_apps = []
+
+# Each item in the list will be shown as an app in the apps page
+# add_to_apps_screen = [
+# 	{
+# 		"name": "rf",
+# 		"logo": "/assets/rf/logo.png",
+# 		"title": "Rf",
+# 		"route": "/rf",
+# 		"has_permission": "rf.api.permission.has_app_permission"
+# 	}
+# ]
 
 # Includes in <head>
 # ------------------
@@ -22,6 +32,13 @@ app_license = "MIT"
 # web_include_css = "/assets/rf/css/rf.css"
 # web_include_js = "/assets/rf/js/rf.js"
 
+# include custom scss in every website theme (without file extension ".scss")
+# website_theme_scss = "rf/public/scss/website"
+
+# include js, css files in header of web form
+# webform_include_js = {"doctype": "public/js/doctype.js"}
+# webform_include_css = {"doctype": "public/css/doctype.css"}
+
 # include js in page
 # page_js = {"page" : "public/js/file.js"}
 
@@ -31,8 +48,10 @@ app_license = "MIT"
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
-doctype_js = {"Task" : "public/js/task.js"}
-
+# Svg Icons
+# ------------------
+# include app icons in desk
+# app_include_icons = "rf/public/icons.svg"
 
 # Home Pages
 # ----------
@@ -42,11 +61,8 @@ doctype_js = {"Task" : "public/js/task.js"}
 
 # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+# 	"Role": "home_page"
 # }
-
-# Website user home page (by function)
-# get_website_user_home_page = "rf.utils.get_home_page"
 
 # Generators
 # ----------
@@ -54,11 +70,42 @@ doctype_js = {"Task" : "public/js/task.js"}
 # automatically create page for each record of this doctype
 # website_generators = ["Web Page"]
 
+# Jinja
+# ----------
+
+# add methods and filters to jinja environment
+# jinja = {
+# 	"methods": "rf.utils.jinja_methods",
+# 	"filters": "rf.utils.jinja_filters"
+# }
+
 # Installation
 # ------------
 
 # before_install = "rf.install.before_install"
 # after_install = "rf.install.after_install"
+
+# Uninstallation
+# ------------
+
+# before_uninstall = "rf.uninstall.before_uninstall"
+# after_uninstall = "rf.uninstall.after_uninstall"
+
+# Integration Setup
+# ------------------
+# To set up dependencies/integrations with other apps
+# Name of the app being installed is passed as an argument
+
+# before_app_install = "rf.utils.before_app_install"
+# after_app_install = "rf.utils.after_app_install"
+
+# Integration Cleanup
+# -------------------
+# To clean up dependencies/integrations with other apps
+# Name of the app being uninstalled is passed as an argument
+
+# before_app_uninstall = "rf.utils.before_app_uninstall"
+# after_app_uninstall = "rf.utils.after_app_uninstall"
 
 # Desk Notifications
 # ------------------
@@ -78,6 +125,14 @@ doctype_js = {"Task" : "public/js/task.js"}
 # 	"Event": "frappe.desk.doctype.event.event.has_permission",
 # }
 
+# DocType Class
+# ---------------
+# Override standard doctype classes
+
+# override_doctype_class = {
+# 	"ToDo": "custom_app.overrides.CustomToDo"
+# }
+
 # Document Events
 # ---------------
 # Hook on document methods and events
@@ -87,7 +142,7 @@ doctype_js = {"Task" : "public/js/task.js"}
 # 		"on_update": "method",
 # 		"on_cancel": "method",
 # 		"on_trash": "method"
-#	}
+# 	}
 # }
 
 # Scheduled Tasks
@@ -105,10 +160,10 @@ doctype_js = {"Task" : "public/js/task.js"}
 # 	],
 # 	"weekly": [
 # 		"rf.tasks.weekly"
-# 	]
+# 	],
 # 	"monthly": [
 # 		"rf.tasks.monthly"
-# 	]
+# 	],
 # }
 
 # Testing
@@ -128,5 +183,62 @@ doctype_js = {"Task" : "public/js/task.js"}
 # along with any modifications made in other Frappe apps
 # override_doctype_dashboards = {
 # 	"Task": "rf.task.get_dashboard_data"
+# }
+
+# exempt linked doctypes from being automatically cancelled
+#
+# auto_cancel_exempted_doctypes = ["Auto Repeat"]
+
+# Ignore links to specified DocTypes when deleting documents
+# -----------------------------------------------------------
+
+# ignore_links_on_delete = ["Communication", "ToDo"]
+
+# Request Events
+# ----------------
+# before_request = ["rf.utils.before_request"]
+# after_request = ["rf.utils.after_request"]
+
+# Job Events
+# ----------
+# before_job = ["rf.utils.before_job"]
+# after_job = ["rf.utils.after_job"]
+
+# User Data Protection
+# --------------------
+
+# user_data_fields = [
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
+# ]
+
+# Authentication and authorization
+# --------------------------------
+
+# auth_hooks = [
+# 	"rf.auth.validate"
+# ]
+
+# Automatically update python controller files with type annotations for this app.
+# export_python_type_annotations = True
+
+# default_log_clearing_doctypes = {
+# 	"Logging DocType Name": 30  # days to retain logs
 # }
 
