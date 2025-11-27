b7d9f60d06aebebcfaede7eb48d42016911c73e4
Resource Factors <it@resourcefactors.com>
2025-11-27 12:49:14 +0400

    hooks updated

--- rf/hooks.py
+++ rf/hooks.py
@@ -26,7 +26,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/rf/css/rf.css"
-app_include_js = ["/assets/rf/js/rf_pre.js", "/assets/rf/js/rf.js", "/assets/rf/js/rf_post.js"]
+app_include_js = "/assets/rf/js/rf.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/rf/css/rf.css"
